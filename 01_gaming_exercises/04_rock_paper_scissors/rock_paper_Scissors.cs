dbf27523fd72ea7c1b4bcac80cdf26803ef2a28a
Nevaeh <s11944972@students.duvalschools.org>
2024-09-16 14:23:23 -0400

    Update rock_paper_Scissors.cs

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_Scissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_Scissors.cs
@@ -3,12 +3,19 @@ class Template {
   static void Main() {
     string playerName = "Test Player";
     // Player and CPU score.
-    int playerScore = 0;
-    int cpuScore = 0;
+    int playerScore;
+    int cpuScore;
     // Player and CPU variable to store choices.
     int playerChoice;
     int cpuChoice;
-    
+    // Allow player to input their name.
+    Console.Writeline("Enter your name:");
+    playerName = Console.Readline();
+    // Output game rules.
+    Console.Writeline("Welcome, " + playerName + "! Let's play Rock, Paper, Scissors.");
+    Console.Writeline("Rules:")
+    Console.Writeline("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.")
+    Console.Writeline("The first player to score 5 points wins.");
     
   }
 }
