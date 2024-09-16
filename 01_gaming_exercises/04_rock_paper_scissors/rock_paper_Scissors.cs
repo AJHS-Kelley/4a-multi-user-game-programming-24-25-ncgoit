f725f08b272ffa1713db6d9ce1312024d8ed7d42
Nevaeh <s11944972@students.duvalschools.org>
2024-09-16 13:51:26 -0400

    Update rock_paper_Scissors.cs

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_Scissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_Scissors.cs
@@ -1,11 +1,13 @@
 using System;
 class Template {
   static void Main() {
-    playerName = "Test Player";
-    playerScore = ""
-    cpuScore = ""
-    playerChoice = ""
-    cpuChoice = ""
+    string playerName = "Test Player";
+    // Player and CPU score.
+    int playerScore = 0;
+    int cpuScore = 0;
+    // Player and CPU variable to store choices.
+    int playerChoice;
+    int cpuChoice;
     
     
   }
